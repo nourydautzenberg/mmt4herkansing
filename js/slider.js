03892b492a4e9fd4c8488fd9547019c0a7ce658c
nourydautzenberg <50704378+nourydautzenberg@users.noreply.github.com>
2019-08-15 17:51:30 +0200

    Add files via upload

--- js/slider.js
+++ js/slider.js
@@ -1,4 +1,4 @@
-var Index = 0;
+var myIndex = 0;
 carousel();
 
 function carousel() {
@@ -7,8 +7,8 @@ function carousel() {
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";  
   }
-  Index++;
-  if (Index > x.length) {myIndex = 1}    
-  x[Index-1].style.display = "block";  
-  setTimeout(carousel, 2000); // Change image every 2 seconds
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 2000);
 }
